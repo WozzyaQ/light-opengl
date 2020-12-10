c74a2373b6289d51b897a61cc5145031fde35faf
Vladyslav <officialwozzya@gmail.com>
2020-12-10 17:53:39 +0200

    basic project

--- cmake-build-debug/CMakeFiles/L3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/L3.dir/DependInfo.cmake
@@ -1,21 +1,10 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/home/vladyslav/CLionProjects/opengl_light/src/glad.c" "/home/vladyslav/CLionProjects/opengl_light/cmake-build-debug/CMakeFiles/L3.dir/src/glad.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../include"
-  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/home/vladyslav/CLionProjects/opengl_light/src/main.cpp" "/home/vladyslav/CLionProjects/opengl_light/cmake-build-debug/CMakeFiles/L3.dir/src/main.cpp.o"
-  "/home/vladyslav/CLionProjects/opengl_light/src/stb_image.cpp" "/home/vladyslav/CLionProjects/opengl_light/cmake-build-debug/CMakeFiles/L3.dir/src/stb_image.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/L3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/L3.dir/cmake_clean.cmake
@@ -1,12 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/L3.dir/src/glad.c.o"
   "CMakeFiles/L3.dir/src/main.cpp.o"
-  "CMakeFiles/L3.dir/src/stb_image.cpp.o"
   "L3"
   "L3.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
+foreach(lang CXX)
   include(CMakeFiles/L3.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
