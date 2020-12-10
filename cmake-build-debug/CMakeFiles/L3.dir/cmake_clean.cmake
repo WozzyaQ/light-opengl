c84cdc5ebe5b9eeba559fe0cc283506e42bbf543
Vladyslav <officialwozzya@gmail.com>
2020-12-10 14:47:39 +0200

    added empty project

---
+++ cmake-build-debug/CMakeFiles/L3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L3.dir/src/glad.c.o"
+  "CMakeFiles/L3.dir/src/main.cpp.o"
+  "CMakeFiles/L3.dir/src/stb_image.cpp.o"
+  "L3"
+  "L3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/L3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
